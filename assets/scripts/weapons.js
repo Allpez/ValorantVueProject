8ba0300bf60c576cf056ee7c1041359ac2c650ea
Samir <samirmh15@gmail.com>
2024-09-04 12:27:33 -0500

    Se creo base de js

--- assets/scripts/weapons.js
+++ assets/scripts/weapons.js
@@ -1,1 +1,21 @@
 let url = "https://valorant-api.com/"
+
+const {createApp} = Vue
+
+const app = createApp({
+    data(){
+        return{
+            
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+
+}).mount('#app')
