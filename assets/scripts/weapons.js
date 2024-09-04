07eb1f26e3b44eda3aa099f1e74158614cc5aa14
Samir <samirmh15@gmail.com>
2024-09-04 12:49:20 -0500

    Se generó createApp

--- assets/scripts/weapons.js
+++ assets/scripts/weapons.js
@@ -5,14 +5,21 @@ const {createApp} = Vue
 const app = createApp({
     data(){
         return{
-            
+            weapons:[],
+            categorys:[],
+            cartegorySelected:[],
+            serchCategory:""
         }
     },
     created(){
 
     },
     methods:{
-
+        getData(url){
+            fetch(url).then(res=>res.json()).then(data =>{
+                this.weapons = data.weapons
+            })
+        }
     },
     computed:{
 
