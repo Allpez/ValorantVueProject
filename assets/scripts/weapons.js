b0b7ebda523295f4306431d6896cfbc0d79f6180
Samir <samirmh15@gmail.com>
2024-09-05 11:29:56 -0500

    Se crearon checkbox dinamicos

--- assets/scripts/weapons.js
+++ assets/scripts/weapons.js
@@ -1,4 +1,4 @@
-let url = "https://valorant-api.com/"
+let urlValorant = "https://valorant-api.com/v1/weapons"
 
 const {createApp} = Vue
 
@@ -6,20 +6,29 @@ const app = createApp({
     data(){
         return{
             weapons:[],
-            categorys:[],
+            categories:[],
             cartegorySelected:[],
             serchCategory:""
         }
     },
     created(){
-
+        this.getData(urlValorant)
+        
+        
     },
     methods:{
         getData(url){
-            fetch(url).then(res=>res.json()).then(data =>{
-                this.weapons = data.weapons
+            fetch(url).then(res=>res.json()).then(data =>{               
+                this.weapons = data.data
+                this.categories = Array.from(new Set(this.weapons.map((weapon)=> weapon.category)) )
+                console.log(this.categories);
+                
+                
             })
-        }
+        },
+        cleanCategoryName(category) {
+            return category.replace('EEquippableCategory::', '');
+          }
     },
     computed:{
 
