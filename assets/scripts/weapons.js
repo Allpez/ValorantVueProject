46ce72b724480181e0eac75d99250bbce3094bf5
Samir <samirmh15@gmail.com>
2024-09-05 13:03:58 -0500

    Se creo buscador, tarjetas dinamicamente  y se les dio orden

--- assets/scripts/weapons.js
+++ assets/scripts/weapons.js
@@ -1,36 +1,34 @@
 let urlValorant = "https://valorant-api.com/v1/weapons"
 
-const {createApp} = Vue
+const { createApp } = Vue
 
 const app = createApp({
-    data(){
-        return{
-            weapons:[],
-            categories:[],
-            cartegorySelected:[],
-            serchCategory:""
+    data() {
+        return {
+            weapons: [],
+            categories: [],
+            cartegorySelected: [],
+            serchCategory: ""
         }
     },
-    created(){
+    created() {
         this.getData(urlValorant)
-        
-        
+
+
     },
-    methods:{
-        getData(url){
-            fetch(url).then(res=>res.json()).then(data =>{               
+    methods: {
+        getData(url) {
+            fetch(url).then(res => res.json()).then(data => {
                 this.weapons = data.data
-                this.categories = Array.from(new Set(this.weapons.map((weapon)=> weapon.category)) )
+                this.categories = Array.from(new Set(this.weapons.map((weapon) => weapon.category)))
                 console.log(this.categories);
-                
-                
             })
         },
         cleanCategoryName(category) {
             return category.replace('EEquippableCategory::', '');
-          }
+        }
     },
-    computed:{
+    computed: {
 
     }
 
