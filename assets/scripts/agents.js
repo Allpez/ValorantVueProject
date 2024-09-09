7232cc307233ec41a20e29f1e18a4d0e89d925c5
JesusVeraPompa <merkapompa@gmail.com>
2024-09-09 13:28:40 -0500

    1.29 Modificaciones de Agents

--- assets/scripts/agents.js
+++ assets/scripts/agents.js
@@ -1,51 +1,31 @@
-console.log("hello");
-let urlAgents = "https://valorant-api.com/v1/agents";
 
-const {createApp} = Vue 
+let urlAgents = 'https://valorant-api.com/v1/agents'
 
-const app= createApp({
-    data()
-        {return{
-            agents:[],
-            agentsBk:[],
+const { createApp } = Vue
+
+const app = createApp({
+    data() {
+        return {
+            agents: [],
+            agentsBk: [],
             categories: [],
             favoritos: [],
             cartegorySelected: [],
-            serchCategory: ""
+            serchCategory: '',
         }
-            
-        
-
     },
-    created(){
+    created() {
         this.bringData(urlAgents)
-        
-        
-
-
     },
-    methods:{
-        bringData(url){
+    methods: {
+        bringData(url) {
             fetch(url)
-            .then(response => response.json())
-            .then(data =>{
-                console.log(data);
-                
-                this.agents = data.data; 
-                console.log(this.agents);
-                
-                this.agentsBk =[...data.data];
-                
-                this.categories = Array.from(new Set(this.agents.map((agent) => agent.role)))
-                console.log(this.categories);
-                
-                
-            })
-        }
-        
+                .then((response) => response.json())
+                .then((d) => {
+                    this.agents = d.data
+                    console.log(this.agents)
+                })
+        },
     },
-    computed:{
-
-    },
+    computed: {},
 }).mount('#app')
-
