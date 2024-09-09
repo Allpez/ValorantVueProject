376ee2bdd390ebff04a349f2497c2ca7e1b5dcd9
jonathan_serrano <jonathan_serrano@hotmail.com>
2024-09-08 23:09:09 -0500

    inicio agents.js creo app, defino info para utilizar para crear checkbox serch

--- assets/scripts/agents.js
+++ assets/scripts/agents.js
@@ -2,3 +2,31 @@ console.log("hello");
 let urlAgents = "https://valorant-api.com/v1/agents";
 
 const {createApp} = vue 
+
+const app= createApp({
+    data()
+        {return{
+            agents:[],
+            roles:{},
+            rolesSelected:[],
+            textSearch:""
+        }
+
+    },
+    created(){
+
+
+    },
+    methods:{
+        bringData(url){
+            fetch(url).then(response => response.json()).then(data =>{
+                this.agents = data.
+            })
+        }
+
+    },
+    computed:{
+
+    },
+}).mount('#app')
+
